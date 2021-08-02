87677197740b72703a8bd2b63c0cab1cf7ccfadb
filipi <filipisee@hotmail.com>
2021-08-02 20:31:18 -0300

    first up

---
+++ app.js
@@ -0,0 +1,20 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector(".modal-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click", () => {
+    if(modalBtn.classList.contains("modal-btn")) {
+        modalOverlay.classList.add("open-modal");
+    }
+});
+
+closeBtn.addEventListener("click", () => {
+    if(modalBtn.classList.contains("modal-btn")) {
+        modalOverlay.classList.remove("open-modal");
+    }
+})
